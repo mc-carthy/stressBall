adb4f96897e7fe5bcfd699b4b90d575eb54e3418
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-09 14:19:44 +0100

    Add basic deformation

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -17,8 +17,31 @@ public class MeshDeformer : MonoBehaviour {
 		vertexVelocities = new Vector3[originalVertices.Length];
 	}
 
+	private void Update () {
+		for (int i = 0; i < displacedVertices.Length; i++) {
+			UpdateVertex (i);
+		}
+		deformingMesh.vertices = displacedVertices;
+		deformingMesh.RecalculateNormals();
+	}
+
 	public void AddDeformingForce (Vector3 point, float force) {
-		Debug.DrawLine (Camera.main.transform.position, point);
+		//Debug.DrawLine (Camera.main.transform.position, point);
+		for (int i = 0; i < displacedVertices.Length; i++) {
+			AddForceToVertex (i, point, force);
+		}
+	}
+
+	private void UpdateVertex (int i) {
+		Vector3 velocity = vertexVelocities [i];
+		displacedVertices [i] += velocity * Time.deltaTime;
+	}
+
+	private void AddForceToVertex (int i, Vector3 point, float force) {
+		Vector3 pointToVertex = displacedVertices [i] - point;
+		float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
+		float velocity = attenuatedForce * Time.deltaTime;
+		vertexVelocities [i] += pointToVertex.normalized * velocity;
 	}
 
 }
