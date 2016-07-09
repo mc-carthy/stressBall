5ed568b131520421bf79f26b1b44ae16401a04bf
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-09 14:28:39 +0100

    Add spring/damping values as well as dealing with scaling forces relative to transform

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -3,9 +3,13 @@
 [RequireComponent(typeof(MeshFilter))]
 public class MeshDeformer : MonoBehaviour {
 
+	public float springForce = 20f;
+	public float dampingForce = 5f;
+
 	private Mesh deformingMesh;
 	private Vector3[] originalVertices, displacedVertices;
 	private Vector3[] vertexVelocities;
+	private float uniformScale = 1f;
 
 	private void Start () {
 		deformingMesh = GetComponent<MeshFilter> ().mesh;
@@ -18,6 +22,7 @@ public class MeshDeformer : MonoBehaviour {
 	}
 
 	private void Update () {
+		uniformScale = transform.localScale.x;
 		for (int i = 0; i < displacedVertices.Length; i++) {
 			UpdateVertex (i);
 		}
@@ -26,6 +31,7 @@ public class MeshDeformer : MonoBehaviour {
 	}
 
 	public void AddDeformingForce (Vector3 point, float force) {
+		point = transform.InverseTransformPoint (point);
 		//Debug.DrawLine (Camera.main.transform.position, point);
 		for (int i = 0; i < displacedVertices.Length; i++) {
 			AddForceToVertex (i, point, force);
@@ -34,11 +40,17 @@ public class MeshDeformer : MonoBehaviour {
 
 	private void UpdateVertex (int i) {
 		Vector3 velocity = vertexVelocities [i];
-		displacedVertices [i] += velocity * Time.deltaTime;
+		Vector3 displacement = displacedVertices [i] - originalVertices [i];
+		displacement *= uniformScale;
+		velocity -= displacement * springForce * Time.deltaTime;
+		velocity *= 1 - dampingForce * Time.deltaTime;
+		vertexVelocities [i] = velocity;
+		displacedVertices [i] += velocity * (Time.deltaTime / uniformScale);
 	}
 
 	private void AddForceToVertex (int i, Vector3 point, float force) {
 		Vector3 pointToVertex = displacedVertices [i] - point;
+		pointToVertex *= uniformScale;
 		float attenuatedForce = force / (1f + pointToVertex.sqrMagnitude);
 		float velocity = attenuatedForce * Time.deltaTime;
 		vertexVelocities [i] += pointToVertex.normalized * velocity;
