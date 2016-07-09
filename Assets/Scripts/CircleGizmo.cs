0227be0061c222a18198dc0d8f32b87811891b9b
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-07-09 13:46:19 +0100

    Detecting correct input for mesh deformation

--- Assets/Scripts/CircleGizmo.cs
+++ Assets/Scripts/CircleGizmo.cs
@@ -21,7 +21,7 @@ public class CircleGizmo : MonoBehaviour {
 
 	private void ShowPoint (float x, float y) {
 		Vector2 square = new Vector2 (x, y);
-		Vector2 circleNonUni = square.normalized;
+		//Vector2 circleNonUni = square.normalized;
 		Vector2 circle;
 		circle.x = square.x * Mathf.Sqrt (1f - square.y * square.y * 0.5f);
 		circle.y = square.y * Mathf.Sqrt (1f - square.x * square.x * 0.5f);
